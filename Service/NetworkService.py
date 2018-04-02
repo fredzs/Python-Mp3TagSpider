192684c6e9019a9eae56f862d04c5dc37d8e3dac
fredzs <fredzhangsheng@gmail.com>
2018-04-02 21:20:12 +0800

    增加cookies

--- Service/NetworkService.py
+++ Service/NetworkService.py
@@ -8,13 +8,24 @@ from Entity.Config import Config
 class NetworkService(object):
     proxies = dict(http='socks5://127.0.0.1:1080', https='socks5://127.0.0.1:1080')
     headers = {
-         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
+    cookies = dict(gid='149691899551074', _unsign_token='749eabe5517bfc2ff915e3af76944952',
+                   cna='+NfADiBhlD8CAXL/KAkvQXvx', bdshare_firstime='1496919006832',
+                   UM_distinctid='160b72b67fe4da-09983bec7af1e2-b7a103e-1fa400-160b72b67ff22c',
+                   _xiamitoken='5a74a13c428c780d55a79a19520e6772', xmgid='ccf5d219-7a5c-43b8-80d3-130b5bbfea9d',
+                   uidXM='24103194', user_from='1',
+                   CNZZDATA921634='cnzz_eid%3D187493467-1514899922-%26ntime%3D1522670548',
+                   __XIAMI_SESSID='294672cd8bfdb6bfd878106e04658a7d', login_method='tblogin',
+                   CNZZDATA2629111='cnzz_eid%3D492183730-1514898251-%26ntime%3D1522674279',
+                   isg='BBMTWz32Tfh6EwElJV_a1MoKopf9YGAkLonNRcUwNjJpRDHmT5zX21TWerQqX_-C',
+                   xm_token='455bb6e74bb2037b317a7ca242d5ffc3',
+                   member_auth='hT%2BeTo1Oums0gveRTdwwcCAY5uCBSGWHlIRS3uIotwVwIYgNNov4x6uTRgpI2yGarGFpVujIsnEN')
 
     @staticmethod
     def get_year_html(album_url):
         logging.debug("开始处理：" + album_url)
-        year_html = requests.get(album_url, headers=NetworkService.headers).text
+        year_html = requests.get(album_url, headers=NetworkService.headers, cookies=NetworkService.cookies).text
         return year_html
 
     @staticmethod
